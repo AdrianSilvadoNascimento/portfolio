0e4fb8763786de3b4b674d60fcd5f222b1409533
Adrian Silva <adrianfsf10@gmail.com>
2022-10-21 08:31:21 -0300

    Adiciona página de projetos

--- portifolio/src/app/app-routing.module.ts
+++ portifolio/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AboutMeComponent } from '../app/main/component/about-me/about-me.component'
+import { ProjectsComponent } from './main/component/projects/projects.component';
 
 const routes: Routes = [
   { path: 'about-me', component: AboutMeComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
