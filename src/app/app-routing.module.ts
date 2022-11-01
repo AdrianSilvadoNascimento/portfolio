ece280ccd358a66f1d0e1f34be9df4316579b690
Adrian Silva <adrianfsf10@gmail.com>
2022-11-01 17:19:51 -0300

    Adiciona página de descrição do projeto com router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProjectDescriptionComponent } from './main/component/projects/project-description/project-description.component';
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AboutMeComponent } from '../app/main/component/about-me/about-me.component'
@@ -6,6 +7,7 @@ import { ProjectsComponent } from './main/component/projects/projects.component'
 const routes: Routes = [
   { path: 'about-me', component: AboutMeComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'projects/:id', component: ProjectDescriptionComponent },
 ];
 
 @NgModule({
