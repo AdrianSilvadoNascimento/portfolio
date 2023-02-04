0742f27ce5a087c5a99971df198f2781c68eaaef
Adrian Silva <adrianfsf10@gmail.com>
2023-02-03 21:00:01 -0300

    atualiza versao angular para 15

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { MatCardModule } from '@angular/material/card'
-import { MatMenuModule } from '@angular/material/menu'
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card'
+import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu'
 import { MatIconModule } from '@angular/material/icon'
-import { MatInputModule } from '@angular/material/input'
-import { MatButtonModule } from '@angular/material/button'
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input'
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatTooltipModule } from '@angular/material/tooltip'
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip'
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field'
 
 @NgModule({
   declarations: [],
